fecab8c08e41f6426a0c3aef673a7f489f4f227a
Devensh22345 <kumardevensh0@gmail.com>
2024-07-21 23:35:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,10 +7,10 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", ""))
-API_HASH     = environ.get("API_HASH", "")
-BOT_TOKEN    = environ.get("BOT_TOKEN", "")
-DATABASE_URI = environ.get("DATABASE_URI", "")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
-ADMIN        = int(environ.get("ADMIN", ""))
-CHANNEL      = environ.get("CHANNEL", "@VJ_Botz")
+API_ID       = int(environ.get("API_ID", "28345030"))
+API_HASH     = environ.get("API_HASH", "2454254a802b8a4eeb3e8ac2816f3fee")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "7305868463:AAE9nSfVJ8uMFtmpRsQ8hjOgs5TbM32A0Bc")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://Autofilterbot:Autofilterbot@cluster0.1oipdqu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1001843643040"))
+ADMIN        = int(environ.get("ADMIN", "5721941544"))
+CHANNEL      = environ.get("CHANNEL", "@DK_ANIME_HINDI")
