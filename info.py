359f3333e5496394455d3607716557679e15ffcf
Devensh22345 <kumardevensh0@gmail.com>
2024-07-22 00:15:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,10 +7,10 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", "28345030"))
-API_HASH     = environ.get("API_HASH", "2454254a802b8a4eeb3e8ac2816f3fee")
+API_ID       = int(environ.get("API_ID", "21509188"))
+API_HASH     = environ.get("API_HASH", "08fcff181f8eb76d4ad5cd63bbba6c9c")
 BOT_TOKEN    = environ.get("BOT_TOKEN", "7305868463:AAE9nSfVJ8uMFtmpRsQ8hjOgs5TbM32A0Bc")
 DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://Autofilterbot:Autofilterbot@cluster0.1oipdqu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1001843643040"))
-ADMIN        = int(environ.get("ADMIN", "6872968794"))
+ADMIN        = int(environ.get("ADMIN", "5721941544"))
 CHANNEL      = environ.get("CHANNEL", "@DKANIME_MOVIES")
